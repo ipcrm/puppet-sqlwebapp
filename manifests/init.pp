033371e347be2ccad73560fe1375aa4b9ebfc578
ipcrm <mcadorette@gmail.com>
2016-11-09 17:03:13 -0500

    switching to archive

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,30 +3,37 @@ class sqlwebapp (
   $dbinstance    = 'MYINSTANCE',
   $dbpass        = 'Azure$123',
   $dbuser        = 'CloudShop',
-  $dbname	 = 'AdventureWorks2012',
+  $dbname         = 'AdventureWorks2012',
   $iis_site      = 'Default Web Site',
   $docroot       = 'C:/inetpub/wwwroot',
   $file_source   = 'https://s3-us-west-2.amazonaws.com/tseteam/files/sqlwebapp',
 ) {
+
   require sqlwebapp::iis
+
   file { "${docroot}/CloudShop":
     ensure  => directory,
   }
   staging::file { 'CloudShop.zip':
     source => "${file_source}/CloudShop.zip",
   }
-  unzip { "Unzip webapp CloudShop":
-    source      => "${::staging_windir}/staging/${module_name}/CloudShop.zip",
-    creates     => "${docroot}/CloudShop/Web.config",
-    destination => "${docroot}/CloudShop",
-    require     => Staging::File['CloudShop.zip'],
-    notify      => Exec['ConvertAPP'],
+
+  archive { 'Unzip webapp CloudShop':
+    path         => $docroot,
+    cleanup      => true,
+    extract      => true,
+    extract_path => "${docroot}/CloudShop",
+    creates      => "${docroot}/Global.asax",
+    require      => File["${docroot}/CloudShop"],
+    notify       => Exec['ConvertAPP'],
   }
+
   file { "${docroot}/CloudShop/Web.config":
     ensure  => present,
     content => template("${module_name}/Web.config.erb"),
-    require => Unzip["Unzip webapp CloudShop"],
+    require => Archive['Unzip webapp CloudShop'],
   }
+
   exec { 'ConvertAPP':
     command     => "ConvertTo-WebApplication \'IIS:/Sites/${iis_site}/CloudShop\'",
     provider    => powershell,
